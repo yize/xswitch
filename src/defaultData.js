0c370298d4eeb7dfa654e3313d3358173fbf83f8
yize <yize.shc@gmail.com>
2018-01-16 00:03:52 +0800

    :bug: fix defaultData

--- src/defaultData.js
+++ src/defaultData.js
@@ -1,6 +1,6 @@
 window.DEFAULT_DATA = `{
   // Use IntelliSense to learn about possible links.
-  // Type `rule` to quick insert rule.
+  // Type \`rule\` to quick insert rule.
   // For more information, visit: https://github.com/yize/xswitch
   "proxy": [
     [
