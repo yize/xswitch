35f45f8284b8d36b30fb5f1b861c610fff680882
yize.shc <hanchao.shc@alibaba-inc.com>
2019-09-04 09:23:17 +0800

    chore: change chrome extension admin url

--- pub.sh
+++ pub.sh
@@ -2,4 +2,4 @@
 rm build/xswitch.css build/xswitch.js build/xswitch.js.map build/background.js.map build/forward.js.map
 rm xswitch.zip
 zip -r xswitch.zip build
-open https://chrome.google.com/webstore/developer/dashboard
+open https://chrome.google.com/webstore/devconsole/g04040876578415358611/idkjhjggpffolpidfkikidcokdkdaogg/edit/package?hl=zh_CN
